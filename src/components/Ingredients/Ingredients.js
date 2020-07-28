bfbcabfeda4f772bddc75d707260c01f752c58f2
Micah Lund <micah.r.lund@gmail.com>
2020-07-28 00:27:46 -0600

    Set up a useCallback in IngredientForm and cleaned up code with additional data rendering.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -7,29 +7,12 @@ const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
   useEffect(() => {
-    fetch('https://react-hooks-update-409ea.firebaseio.com/ingredients.json')
-    .then(response => {
-      return response.json();
-    }).then(responseData => {
-      const loadedIngredients = [];
-      for (const key in responseData){
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        })
-      }
-      setUserIngredients(loadedIngredients);
-    })
-  }, []);
+    console.log('RENDERING INGREDIENTS', userIngredients);
+  }, [userIngredients]);
 
-  // useEffect(() => {
-  //   console.log('RENDERING INGREDIENTS', userIngredients);
-  // }, [userIngredients]);
-
-  const filteredIngredientsHandler = filteredIngredients => {
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
     setUserIngredients(filteredIngredients);
-  }
+  }, [])
 
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-update-409ea.firebaseio.com/ingredients.json', {
