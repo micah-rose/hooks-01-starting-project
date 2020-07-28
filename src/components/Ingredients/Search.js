77868021d75394cc9ff1d8568e7c012495b10129
Micah Lund <micah.r.lund@gmail.com>
2020-07-28 00:37:01 -0600

    Set up a cleanup function within useEffect.

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -9,7 +9,7 @@ const Search = React.memo(props => {
   const inputRef = useRef();
 
   useEffect(() => {
-    setTimeout (() => {
+    const timer = setTimeout (() => {
       if(enteredFilter === inputRef.current.value){
         const queryParams = enteredFilter.length === 0 
         ? '' : `?orderBY="title"&equalTo="${enteredFilter}"`;
@@ -30,6 +30,9 @@ const Search = React.memo(props => {
         })
       }
     }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
   }, [enteredFilter, onLoadIngredients, inputRef]);
     
   return (
